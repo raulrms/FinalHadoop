973fd23e3852fa1b0e6bbdd3a37d463645e051ee
Raúl Martínez <raulrms@yahoo.es>
2016-12-04 10:32:46 +0100

    Create reduce.py

---
+++ reduce.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import sys  
+(name, lastid) = (None,None)
+for line in sys.stdin:
+	(id, fall, year, reclat,reclong) = line.split(',') 
+	if lastid == id:
+		print '%s\t%s\%s' % (name, reclat, reclong)
